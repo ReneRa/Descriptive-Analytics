699db6b44220c298541238e290eaf0235c03eafa
ReneRa <rene.rauch@outlook.de>
2017-05-13 13:48:37 +0100

    new class

--- PLSPM_Algorithm.R
+++ PLSPM_Algorithm.R
@@ -1,6 +1,14 @@
 PLSPM <- function(data,)
   
-data <- scale(rnorm(data[2:25]))
+
+data1 = data[2:25]
+data1 <- scale(data1) 
+
+
+
+#Check by colMeans(data1,na.rm= TRUE)
+      #diag(var(data1, na.rm= TRUE))
+
 ###Step1 Initialization
   
   
