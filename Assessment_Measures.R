8490375501867bdc8daf78ab9f0572d2c7f9f829
andrewtalas <andrewtalas@gmail.com>
2017-06-08 14:46:07 +0100

    communality Index update + avg communality Index

--- Assessment_Measures.R
+++ Assessment_Measures.R
@@ -1,23 +1,36 @@
-#assessment measures 
+#Assessment measures 
 
 #Communality - measures part of the variance that is common between both a latent variable and it's indicator;
 #evaluating how well the indicators are explained by its latent variable.
-
+#Calculated as the square of the standardized outerLoadings
+#HOWTO: standardize columns: (outerLoadings / colSums(outerLoadings)[col(outerLoadings)])
 communalityIndex <- function(){
-  LVScores = finalResult$LVScores
   outerLoadings = finalResult$outerLoadings
-  for(i in LVScores){
-    communality[i] = outerLoadings^2
-  }
-  return(communality)
+  communalityIndex = outerLoadings^2
+  return(communalityIndex)
 }
 
+avgCommunalityIndex <- function(communalityIndex){
+  avgCommunalityIndex = matrix(0, nrow = 1, ncol = ncol(communalityIndex)); colnames(avgCommunalityIndex) = colnames(communalityIndex)
+  nonZeroCols = colSums(communalityIndex != 0)
+  for (i in 0:ncol(avgCommunalityIndex)){
+    sum(communalityIndex[,i]/nonZeroCols)
+  }
+return(avgCommunalityIndex)
+}
+#TODO: Print "." instead of zero's. Print average value for each outerLoading variable. Print overll avg.
+
+
 #rSquared = the amount of variance in the endogenous LV, explained by its independent LVs
-rSquared <- function()
+rSquared <- function(){
   LVScores = finalResult$LVScores
   endLVscore = LVScores %*% pathCoefficients
 # (LVscore - meanScore)^2 / (predecessorLVscores - meanScore)^2 
   rSquared = as.matrix(apply(endLVscore, 2, var) / apply(LVScores, 2, var))
-  
+  return(rSquared)
+}
 
-  
+
+#Goodness of Fit (GOF) index an quality assessment for structural and measurement models-
+#Takes into account communality - making it more applicable to reflective indicators, rather than formative
+#Calculated as the geometric of the average communality and the average rSquared value
